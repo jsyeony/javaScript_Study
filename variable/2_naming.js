75667c25efcb130335f7051985eb873e725b03a5
jsyeony <skeo4864@gmail.com>
2022-08-11 18:05:39 +0900

    variable_naming

---
+++ variable/2_naming.js
@@ -0,0 +1,31 @@
+// 주석
+/**
+ * 변수 규칙!
+ * 라틴문자(0-9, a-z, A-Z), _
+ * 대소문자를 구분함
+ * 추천: camelCase (likeThis)
+ * 한국어 XXXX
+ * 예약어 XXXX
+ * 숫자로 시작 XXXX
+ * 특수문자 XX (_, $ 두가지는 예외)
+ * 이모지 XXXX
+ * 여러개의 변수를 1,2,3 숫자로 구분 X -> 최대한 의미있게 써주는것이 좋음
+ */
+
+let apple;
+let redApple;
+
+// 나쁜예제
+let num = 20;
+let audio1;
+let audio2;
+
+// 좋은예제
+let myAge = 20;
+let backgroundAudio;
+let windAudio;
+
+// 꿀팁! - 말하고자하는 중복되는 단어를 먼저 써주고 뒤에 달라지는 종류를 써주기
+let audioBackground;
+let audioWind;
+
