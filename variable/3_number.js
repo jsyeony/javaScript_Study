fad8551ebf75bcc855dc75a030633ec30824f996
jsyeony <skeo4864@gmail.com>
2022-08-11 18:05:51 +0900

    Type_number

---
+++ variable/3_number.js
@@ -0,0 +1,12 @@
+let integer = 123; // 정수
+let negative = -123; // 음수
+let double = 1.23; // 실수
+
+let binary = 0b1111011; // 2진수
+let octal = 0o173; // 8진수
+let hex = 0x7b; // 16진수
+
+console.log(0 / 123); // 0
+console.log(123 / 0); // Infinity
+console.log(123 / -0); // -Infinity
+console.log(123 / 'text'); // (NaN Not a number)
